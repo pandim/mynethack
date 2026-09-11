6e15ed0e6e5f0c44fc43588cdd96ddfbf9f4d2e6
Dmitry Panfilov <d.panfilov@inbox.ru>
2026-09-11 16:01:47 +0300

    Сытость -2 на ход

--- game/player.go
+++ game/player.go
@@ -71,7 +71,7 @@ func (p *Player) Move(dx, dy int) {
 
 	p.X += dx
 	p.Y += dy
-	p.Hunger += 5
+	p.Hunger += 2
 
 	if p.logger != nil {
 		p.logger.Printf("MOVE: Игрок переместился на (%d, %d). Голод: %d", p.X, p.Y, p.Hunger)
